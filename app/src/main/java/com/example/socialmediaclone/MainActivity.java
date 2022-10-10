e72be0db9cda27e8c4f5e3fe69428f847d0b99f7
karthik rajan <karthikravi2630@gmail.com>
2022-10-10 21:10:32 +0530

    Firebase activity is created with log in activity

---
+++ app/src/main/java/com/example/socialmediaclone/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.socialmediaclone;
+
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG="MainActivity";
+    private FirebaseAuth mAuth;
+    private EditText mEmail,mPassword;
+    public void signUpClick(View view){
+        String email=mEmail.getText().toString();
+        String password=mPassword.getText().toString();
+        signUp(email,password);
+    }
+
+    public void loginClick(View view){
+        String email=mEmail.getText().toString();
+        String password=mPassword.getText().toString();
+        logIn(email,password);
+    }
+
+    private void signUp(String email,String password){
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(MainActivity.this, "SignIn Succesfull.",
+                                    Toast.LENGTH_SHORT).show();
+                            Log.i(TAG,"User is "+user.getEmail());
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void logIn(String email,String password){
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(MainActivity.this, "LogIn Successfull",
+                                    Toast.LENGTH_SHORT).show();
+                            Log.i(TAG,"User is "+user.getEmail());
+
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth=FirebaseAuth.getInstance();
+        mEmail=findViewById(R.id.editTextTextEmailAddress);
+        mPassword=findViewById(R.id.editTextTextPassword);
+    }
+}
